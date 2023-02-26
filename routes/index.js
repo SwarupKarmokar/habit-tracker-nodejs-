634591627bddd9bd607930ed478cd865e4e5cb4a
Swarup Karmokar <swarupkarmokar123@gmail.com>
2023-02-26 13:07:19 +0530

    all file added for habitracker app

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+// check whether router is loaded  or not
+// console.log('router is loaded');
+
+//controller for routing
+const homeController = require('../controllers/HomeController');
+
+router.get('/', passport.checkAuthentication, homeController.home);
+
+// router.get('/' , homeController.home);
+router.use('/users', require('./UserRoutes'));
+
+
+
+
+module.exports = router;
