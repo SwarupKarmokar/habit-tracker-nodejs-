00dcb449e1acab21d99377b956a4f2ab4659730b
Swarup Karmokar <swarupkarmokar123@gmail.com>
2023-03-24 20:52:26 +0530

    error handled

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(process.env.MONGO_URL)
     .then(console.log("Connected"))
     .catch((err) => console.log(err));
